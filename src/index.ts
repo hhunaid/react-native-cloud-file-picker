e653c9f6cc535fe22ba5b383370b58ee2f1d20db
Hunaid Hassan <hhunaid@gmail.com>
2021-04-05 21:52:32 +0200

    0.1.2

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { NativeModules } from 'react-native';
 
 const { CloudPicker: RCTCloudPicker } = NativeModules;
 
-type ThumbnailType = '640x480' | '64x64' | '200x200'
+type ThumbnailType = '640x480' | '64x64' | '200x200';
 
 type CloudPickerResponse = {
-    uri: string,
-    thumbnails?: Record<ThumbnailType, string>
-}
+  uri: string;
+  thumbnails?: Record<ThumbnailType, string>;
+};
 
-export default class CloudPicker { 
-    public static pickDropBoxFile(appKey: string): Promise<CloudPickerResponse> {
-        return RCTCloudPicker.pickDropBoxFile(appKey);
-    }
+export default class CloudPicker {
+  public static pickDropBoxFile(appKey: string): Promise<CloudPickerResponse> {
+    return RCTCloudPicker.pickDropBoxFile(appKey);
+  }
 }
